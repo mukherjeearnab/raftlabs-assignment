d9b1008f240cdd59fea37d614fa7b87022158c60
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-04-18 19:49:22 +0530

    Fixed problems with printing the table

--- modules/table/index.js
+++ modules/table/index.js
@@ -1,12 +1,18 @@
-var Table = require("cli-table");
+const truncate = (str) => {
+    if (str.length > 10) return str.slice(0, 10) + "...";
+
+    return str;
+};
 
 module.exports = (data) => {
     const output = data.map((item) => {
-        if (item.description) item.description = item.description.slice(0, 10) + "...";
-        if (item.title) item.title = item.title.slice(0, 10) + "...";
+        const content = JSON.parse(JSON.stringify(item));
+
+        if (content.description) content.description = truncate(content.description);
+        if (content.title) content.title = truncate(content.title);
         // if (item.description) item.description = item.description.slice(0, 10) + "...";
 
-        return item;
+        return content;
     });
     console.table(output);
 };
